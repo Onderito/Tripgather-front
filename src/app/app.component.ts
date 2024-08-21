3c96930cf5deb4b04fbcc80f0b168aedaef88e42
Onderito <ulas.onder@outlook.fr>
2024-08-21 15:19:48 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PRIMENG } from '../primeNgImport';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PRIMENG],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'tripgather-front';
---
+++ src/primeNgImport.ts
@@ -0,0 +1,19 @@
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { AccordionModule } from 'primeng/accordion';
+import { CarouselModule } from 'primeng/carousel';
+import { TagModule } from 'primeng/tag';
+import { MenubarModule } from 'primeng/menubar';
+import { TableModule } from 'primeng/table';
+import { InputNumberModule } from 'primeng/inputnumber';
+
+export const PRIMENG = [
+  ButtonModule,
+  InputTextModule,
+  AccordionModule,
+  CarouselModule,
+  TagModule,
+  MenubarModule,
+  TableModule,
+  InputNumberModule,
+];
