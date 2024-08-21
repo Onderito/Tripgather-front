b225ef0c191b8f8a25a7897ce140c879cb2c6c14
theo sarhane <theosarhane@MacBook-Pro-de-theo.local>
2024-08-21 16:20:33 +0200

    ajouterles font du projet

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+
+    },
+    fontFamily : {
+      mont : ["Montserrat"],
+      lexend : ["Lexend"]
+    }
   },
   plugins: [],
 };
