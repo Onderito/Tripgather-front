3362da5f49a5ab44cf00baf69730aad162f90c45
2z75 <zinedine.chader@gmail.com>
2024-08-22 14:25:43 +0200

    initialisation

---
+++ src/app/shared/components/button/button.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss'
+})
+export class ButtonComponent {
+  @Input() text: string = 'Button'; // Texte du bouton
+  @Input() type: string = 'button'; // Type du bouton
+  @Input() classes: string = ''; // Classes CSS supplémentaires
+  @Input() variant: 'primary' | 'danger' = 'primary'; // Choisir un variant de classe
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,13 +9,38 @@ module.exports = {
       },
       colors: {
         primary: "#7CD466",
-        white : "FFFFFF",
-        black : "020202",
-        red : "F57C7C",
-        grey : "828282",
-        inputBorder : "CCCCCC",
-        categoryColor : "F7FBFB"
+        white : "#FFFFFF",
+        black : "#020202",
+        red : "#F57C7C",
+        grey : "#828282",
+        inputBorder : "#DDDDDD",
+        categoryColor : "#F7FBFB"
       },
+    },
+  },
+  fontSize: {
+    // Tailles de police personnalisées
+    xs: ['0.75rem', { lineHeight: '1rem' }],  // 12px
+    sm: ['0.875rem', { lineHeight: '1.25rem' }], // 14px
+    base: ['1rem', { lineHeight: '1.5rem' }],  // 16px
+    lg: ['1.125rem', { lineHeight: '1.75rem' }], // 18px
+    xl: ['1.25rem', { lineHeight: '1.75rem' }], // 20px
+    '2xl': ['1.5rem', { lineHeight: '2rem' }], // 24px
+    '3xl': ['1.875rem', { lineHeight: '2.25rem' }], // 30px
+    '4xl': ['2.25rem', { lineHeight: '2.5rem' }], // 36px
+    '5xl': ['3rem', { lineHeight: '1' }], // 48px
+    '6xl': ['3.75rem', { lineHeight: '1' }], // 60px
+    '7xl': ['4.5rem', { lineHeight: '1' }], // 72px
+  },
+  container: {
+    center: true, // Centre le conteneur sur tous les écrans
+    padding: {
+      DEFAULT: '1rem', // Padding par défaut pour tous les écrans
+      sm: '2rem',      // Padding pour les écrans `sm` (640px et plus)
+      md: '2rem',      // Padding pour les écrans `md` (768px et plus)
+      lg: '4rem',      // Padding pour les écrans `lg` (1024px et plus)
+      xl: '5rem',      // Padding pour les écrans `xl` (1280px et plus)
+      '2xl': '6rem',   // Padding pour les écrans `2xl` (1536px et plus)
     },
   },
   plugins: [],
