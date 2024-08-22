e1cbc8c6714a9d0c3910f5ec7386d7175c5e5c88
theo sarhane <theosarhane@MacBook-Pro-de-theo.local>
2024-08-22 10:38:07 +0200

    ajout de couleur

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,21 @@ module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {
-
+      fontFamily: {
+        mont: ["Montserrat"],
+        lexend: ["Lexend"],
+      },
+      colors: {
+        primary: "#7CD466",
+        white : "FFFFFF",
+        black : "020202",
+        red : "F57C7C",
+        grey : "828282",
+        inputBorder : "CCCCCC",
+        categoryColor : "F7FBFB"
+      },
     },
-    fontFamily : {
-      mont : ["Montserrat"],
-      lexend : ["Lexend"]
-    }
   },
   plugins: [],
 };
+
