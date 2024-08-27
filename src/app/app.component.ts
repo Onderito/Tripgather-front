e28a1eafd1e34709142906b499c6532e801db6e0
Onderito <ulas.onder@outlook.fr>
2024-08-27 17:00:29 +0200

    navBar done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PRIMENG } from '../primeNgImport';
+import { NavBarComponent } from './layout/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PRIMENG],
+  imports: [RouterOutlet, PRIMENG, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
