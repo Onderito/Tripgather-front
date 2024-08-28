55f544ac256050e56ec811f6b62f7a91348c8799
Onderito <ulas.onder@outlook.fr>
2024-08-28 11:30:27 +0200

    ajout d'assets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PRIMENG } from '../primeNgImport';
 import { NavBarComponent } from './layout/nav-bar/nav-bar.component';
+import { HomePageComponent } from './page/home-page/home-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PRIMENG, NavBarComponent],
+  imports: [RouterOutlet, PRIMENG, NavBarComponent, HomePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
