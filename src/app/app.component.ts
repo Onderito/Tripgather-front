5de276497649e6e710a703b6106b4785cefc7eb8
Onderito <ulas.onder@outlook.fr>
2024-08-28 11:34:39 +0200

    modif app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PRIMENG } from '../primeNgImport';
-import { NavBarComponent } from './layout/nav-bar/nav-bar.component';
-import { HomePageComponent } from './page/home-page/home-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PRIMENG, NavBarComponent, HomePageComponent],
+  imports: [RouterOutlet, PRIMENG],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
