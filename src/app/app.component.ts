ca5ce0e9abdfc507ae05c5c3fb3ef068820cdddd
Onderito <ulas.onder@outlook.fr>
2024-08-29 12:01:33 +0200

    homePage desktop fait

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PRIMENG } from '../primeNgImport';
+import { HomePageComponent } from './page/home-page/home-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PRIMENG],
+  imports: [RouterOutlet, PRIMENG, HomePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
