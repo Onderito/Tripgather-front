81b8f7f579ee0928c3f0f92dd31870cf987a5830
Onderito <ulas.onder@outlook.fr>
2024-08-29 14:13:30 +0200

    suppression d'import dans l'app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PRIMENG } from '../primeNgImport';
-import { HomePageComponent } from './page/home-page/home-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PRIMENG, HomePageComponent],
+  imports: [RouterOutlet, PRIMENG],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
