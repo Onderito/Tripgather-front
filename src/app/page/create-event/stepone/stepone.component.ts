0513be5c2449d3a5564fad9860959de1a39885be
jean519 <129189905+jean519@users.noreply.github.com>
2024-10-09 10:16:58 +0200

    merge other branch

--- src/app/page/create-event/stepone/stepone.component.ts
+++ src/app/page/create-event/stepone/stepone.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, ValidationErrors } from '@angular/forms';
 import { FormeventService } from '../../../core/service/formevent.service.js';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
@@ -16,16 +16,43 @@ import { NavBarComponent } from "../../../layout/nav-bar/nav-bar.component";
 })
 export class SteponeComponent {
   form: FormGroup;
+  minDateBack: Date | null = null;
+  gender : string[] = ['woman','man']
 
   constructor(private formEvent: FormeventService, private router: Router, private fb: FormBuilder) {
     this.form = this.fb.group({
-      title : ['', [Validators.required]],
+      title: ['', [Validators.required]],
       start: ['', Validators.required],
       back: ['', Validators.required],
       budget: ['', Validators.required],
-      nbMenber : ['', Validators.required],
-      gender : ['', Validators.required],
+      nbMenber: ['', Validators.required],
+      gender: ['', Validators.required],
+    }, {
+      validators: this.dateValidator // Ajout de la validation croisée
     });
+
+    // Écoutez les changements sur le champ 'start' pour mettre à jour minDate de 'back'
+    this.form.get('start')?.valueChanges.subscribe((startValue: Date) => {
+      if (startValue) {
+        this.minDateBack = startValue;
+      }
+    });
+  }
+
+  dateValidator(group: FormGroup): ValidationErrors | null {
+    const start = group.get('start')?.value;
+    const back = group.get('back')?.value;
+  
+    if (start && back && back < start) {
+      return { backBeforeStart: true };
+    }
+  
+    if (!start && back) {
+      group.get('back')?.reset();
+      return { backBeforeStart: true }; 
+    }
+  
+    return null; // Si tout est correct, retourne null
   }
 
   sendData() {
