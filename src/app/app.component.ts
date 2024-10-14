9864a162ce88690cd4229e04c43b9dbeb55cd2f5
2z75 <zinedine.chader@gmail.com>
2024-10-14 10:53:22 +0200

    ajout du block notif demande de participation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { PRIMENG } from '../primeNgImport';
 import { FooterComponent } from './layout/footer/footer.component';
 import { SecondaryCardComponent } from './shared/components/secondary-card/secondary-card.component';
+import { NotificationPageComponent } from './page/notification-page/notification-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PRIMENG, FooterComponent, SecondaryCardComponent],
+  imports: [RouterOutlet, PRIMENG, FooterComponent, SecondaryCardComponent, NotificationPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
