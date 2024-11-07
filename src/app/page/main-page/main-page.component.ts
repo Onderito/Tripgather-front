9e7159a64e6b0200e6e84e49685c93ff7ba500b0
Onderito <ulas.onder@outlook.fr>
2024-11-07 08:15:50 +0100

    responsive main page

--- src/app/page/main-page/main-page.component.ts
+++ src/app/page/main-page/main-page.component.ts
@@ -2,16 +2,18 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MainCardComponent } from '../../shared/components/main-card/main-card.component';
 import { CommonModule } from '@angular/common';
+import { ButtonComponent } from '../../shared/components/button/button.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [MainCardComponent, CommonModule],
+  imports: [MainCardComponent, CommonModule, ButtonComponent],
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.scss']
+  styleUrls: ['./main-page.component.scss'],
 })
 export class MainPageComponent {
   public eventData: any[] = [];
+  isDropdownVisible = false;
   instances = Array(3);
   search: FormGroup;
 
@@ -22,7 +24,11 @@ export class MainPageComponent {
       state: [''],
       sexe: [''],
       start: [''],
-      back: ['']
+      back: [''],
     });
+  }
+
+  toggleDropdown() {
+    this.isDropdownVisible = !this.isDropdownVisible;
   }
 }
