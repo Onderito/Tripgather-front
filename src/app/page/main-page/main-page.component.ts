2680c4f1972a205bc0b5b1da5ad35ee6e79c01db
Onderito <ulas.onder@outlook.fr>
2024-12-11 16:54:28 +0100

    main page css fix

--- src/app/page/main-page/main-page.component.ts
+++ src/app/page/main-page/main-page.component.ts
@@ -2,27 +2,32 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MainCardComponent } from '../../shared/components/main-card/main-card.component';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
   imports: [MainCardComponent, CommonModule],
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.scss']
+  styleUrls: ['./main-page.component.scss'],
 })
 export class MainPageComponent {
   public eventData: any[] = [];
   instances = Array(3);
   search: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private Route: Router) {
     // Initialisez le formulaire dans le constructeur
     this.search = this.fb.group({
       city: [''],
       state: [''],
       sexe: [''],
       start: [''],
-      back: ['']
+      back: [''],
     });
+  }
+
+  onChangeRoute(url: string) {
+    this.Route.navigate([url]);
   }
 }
