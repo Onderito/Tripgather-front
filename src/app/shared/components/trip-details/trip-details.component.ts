eb897fbcd5288492106ae4728d7814955c5d60c2
Onderito <ulas.onder@outlook.fr>
2025-01-07 17:06:36 +0100

    rajout du favori pour les card

--- src/app/shared/components/trip-details/trip-details.component.ts
+++ src/app/shared/components/trip-details/trip-details.component.ts
@@ -3,6 +3,8 @@ import { ButtonComponent } from '../utils/button/button.component';
 import { HostProfileComponent } from '../host-profile/host-profile.component';
 import { ParticipantsListViewComponent } from '../participants-list-view/participants-list-view.component';
 import { SecondaryCardComponent } from '../secondary-card/secondary-card.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-trip-details',
@@ -12,6 +14,7 @@ import { SecondaryCardComponent } from '../secondary-card/secondary-card.compone
     HostProfileComponent,
     ParticipantsListViewComponent,
     SecondaryCardComponent,
+    CommonModule,
   ],
   templateUrl: './trip-details.component.html',
   styleUrl: './trip-details.component.scss',
@@ -19,4 +22,12 @@ import { SecondaryCardComponent } from '../secondary-card/secondary-card.compone
 export class TripDetailsComponent {
   imgOfTrip: string = '/assets/imgs/homeImg.png';
   imgOfhost: string = '/assets/imgs/Ulas.png';
+  heart: string = '/assets/imgs/white-heart.svg';
+
+  liked: boolean = false;
+
+  toggleLike() {
+    this.liked = !this.liked;
+    console.log('Liked:', this.liked); // Ajoutez ce log pour déboguer
+  }
 }
