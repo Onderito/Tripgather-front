1d345f23878a6941aa66cc01d295f28a76b2b55c
theo sarhane <theosarhane@MacBook-Pro-de-theo.local>
2025-01-08 15:56:21 +0100

    logged for participated event

--- src/app/shared/components/trip-details/trip-details.component.ts
+++ src/app/shared/components/trip-details/trip-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { ButtonComponent } from '../utils/button/button.component';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../core/service/auth.service';
 
 @Component({
   selector: 'app-trip-details',
@@ -17,4 +19,23 @@ export class TripDetailsComponent {
   imgOfhost: string = '/assets/imgs/Ulas.png';
   @Input() userDetail : any;
 
+  isLogged: boolean = false;
+  participe: string = 'Participer';
+  
+  constructor(private router: Router, private authService: AuthService) {}
+
+  ngOnInit() {
+    this.authService.isAuthenticated.subscribe((isLog) => {
+      this.isLogged = isLog
+    })
+  }
+  
+  redirection() {
+    if (!this.isLogged) {
+      this.router.navigate(['/auth/login']);
+    } else {
+      this.participe = "Vous participez";
+    }
+  }
+  
 }
