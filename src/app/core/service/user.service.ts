6ecd00497818ba42ce685dec63a3f52471052201
theo sarhane <theosarhane@MacBook-Pro-de-theo.local>
2025-02-05 10:38:16 +0100

    login detail user done

--- src/app/core/service/user.service.ts
+++ src/app/core/service/user.service.ts
@@ -1,13 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, Observable, throwError } from 'rxjs';
+import { BehaviorSubject, catchError, Observable, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
+    private userSubject = new BehaviorSubject<any>({});
+    public user$ = this.userSubject.asObservable();
+
   constructor(private http : HttpClient) { }
+
+  public upadateValue(user : any ) {
+    this.userSubject.next(user)
+  }
+
+  public getUser(): Observable<any> {
+    return this.user$;
+  }
 
     getOneUser(): Observable<any> {
       return this.http.get<any>('/users/1').pipe(
