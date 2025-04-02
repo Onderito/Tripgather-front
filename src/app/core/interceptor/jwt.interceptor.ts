abf7b2d773a6a6f15a63026509f1a367b31fd1b6
Onderito <ulas.onder@outlook.fr>
2025-04-02 17:14:35 +0200

    déploiement

--- src/app/core/interceptor/jwt.interceptor.ts
+++ src/app/core/interceptor/jwt.interceptor.ts
@@ -1,13 +1,16 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { JwtService } from '../service/jwt.service';
+import { environment } from '../../../environments/environment';
 
 export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
   const jwtService = inject(JwtService); // Injection manuelle en standalone
-  const API_URL = 'http://localhost:8080/api';
+  const API_URL = `${environment.apiUrl}/api`;
 
   // Ajouter l'URL de base pour les requêtes non liées aux assets
-  let apiReq = req.url.includes('assets') ? req : req.clone({ url: API_URL + req.url });
+  let apiReq = req.url.includes('assets')
+    ? req
+    : req.clone({ url: API_URL + req.url });
 
   // Ajouter le token si disponible
   const token = jwtService.getToken();
